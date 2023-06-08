67a432e04600572767127ec6fdddaa3e42cba014
denisse.ortiz-at-737553383013 <denisse.ortiz@kiritek.com>
2023-06-08 16:22:11 -0600

    Otro commit, eliminar lineas innecesarias

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var express = require('express');
 var path = require('path');
 var flash = require('express-flash');
 var session = require('express-session');
-var mysql = require('mysql');
-//var connection  = require('./lib/db');
+
 var MaterialesRouter = require('./routes/materiales');
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
